bf053f0ecf4c00c0cd550661a4162b8fa45467ed
Matt Dunbar <matt@mattdsworld.com>
2015-05-15 19:18:26 -0400

    Update tax_cloud.rb

--- app/models/spree/tax_cloud.rb
+++ app/models/spree/tax_cloud.rb
@@ -14,7 +14,7 @@ module Spree
 
       index = -1 # array is zero-indexed
       # Prepare line_items for lookup
-      order.line_items.where('quantity > 0').each { |line_item| transaction.cart_items << cart_item_from_item(line_item, index += 1) }
+      order.line_items.where('quantity > 0').where('price > 0').each { |line_item| transaction.cart_items << cart_item_from_item(line_item, index += 1) }
       # Prepare shipments for lookup
       order.shipments.each { |shipment| transaction.cart_items << cart_item_from_item(shipment, index += 1) }
       transaction
